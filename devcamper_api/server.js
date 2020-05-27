edaf5eb0f2c56c470a155214126801b083b177af
Gekif <dzulfikar.maulana@gmail.com>
2020-05-27 21:11:07 +0700

    3. Starting Our DevCamper Project: 3. Creating Routes  Responses In Express

--- devcamper_api/server.js
+++ devcamper_api/server.js
@@ -6,6 +6,57 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.get('/api/v1/bootcamps', (req, res) => {
+    res.status(200)
+        .json({
+            success: true,
+            data: {
+                msg: 'Show all bootcamps'
+            }
+        });
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200)
+        .json({
+            success: true,
+            data: {
+                msg: `Get bootcamp ${req.params.id}`
+            }
+        });
+});
+
+
+app.post('/api/v1/bootcamps', (req, res) => {
+    res.status(200)
+        .json({
+            success: true,
+            data: {
+                msg: 'Create new bootcamp'
+            }
+        });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200)
+        .json({
+            success: true,
+            data: {
+                msg: `Update bootcamp ${req.params.id}`
+            }
+        });
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200)
+        .json({
+            success: true,
+            data: {
+                msg: `Delete bootcamp ${req.params.id}`
+            }
+        });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
