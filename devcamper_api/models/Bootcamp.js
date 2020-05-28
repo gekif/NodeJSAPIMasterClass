c6fbb9329195733c49a9514335b6eedcacfcfd4a
Gekif <dzulfikar.maulana@gmail.com>
2020-05-28 14:32:41 +0700

    4. Getting Started With MongoDB  Bootcamps Resource: 5. Create Bootcamp - POST

--- devcamper_api/models/Bootcamp.js
+++ devcamper_api/models/Bootcamp.js
@@ -111,12 +111,6 @@ const BootcampSchema = new mongoose.Schema({
     createdAt: {
         type: Date,
         default: Date.now
-    },
-
-    user: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'User',
-        required: true
     }
 });
 
